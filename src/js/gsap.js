f9ed8d5305fa7dacaa4f15c1dfd7e2b9eb7e2579
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2026-02-09 01:01:21 -0600

    added scrolltrigger and splittext to hero section

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,4 +1,13 @@
 
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(SplitText, ScrollTrigger);
 
-let split = SplitText.create("#split", { type: "words, chars" });
+let mySplitText = SplitText.create("#split", { type: "words, chars" });
+
+let chars = mySplitText.chars;
+
+gsap.from(chars, {
+    yPercent: 200,
+    stagger: 0.02,
+    ease: 'back.out',
+    duration: 2,
+})
