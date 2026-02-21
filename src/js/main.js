2770eabfda68941992ef90b71592c3dada512203
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2026-02-21 14:44:39 -0600

    changed hero section into a video, added carousel, but having issues with carousel skipping

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+ //Get all carousels on the page
+ const carousels = document.querySelectorAll('.infiniteCarousel');
 
-
-// alert("working");
+ //Loop thorugh each carousel
+ carousels.forEach(carousel => {
+    //Get the inner div of each carousel
+    const carouselInner = carousel.querySelector('.infiniteCarousel div');
+    //Get the inner content (images) from the carousel
+    const carouselContent = Array.from(carouselInner.children);
+    //Loop through each item (image)
+    carouselContent.forEach(item =>{
+        //Duplicate each item
+        const duplicateItem = item.cloneNode(true);
+        //append the duplicated item to the carousel
+        carouselInner.appendChild(duplicateItem);
+        //add carousel animation
+        carouselInner.style.animation = "move 12s linear infinite";
+    });
+ });
--- src/js/vanta.js
+++ src/js/vanta.js
@@ -1,15 +1,15 @@
 
-window.addEventListener('load', () => {
-VANTA.RINGS({
-  el: "#hero",
-  mouseControls: false,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  backgroundColor: 0x0,
-  color: 0xff0000
-})
-});
+// window.addEventListener('load', () => {
+// VANTA.RINGS({
+//   el: "#hero",
+//   mouseControls: false,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   backgroundColor: 0x0,
+//   color: 0xff0000
+// })
+// });
